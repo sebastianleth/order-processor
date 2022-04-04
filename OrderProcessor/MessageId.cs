b93bb8ee9234c5b944757ae395cd3a07f86d58c2
Sebastian Leth <sebastian@koderi.dk>
2022-04-04 22:44:50 +0200

    More

--- OrderProcessor/MessageId.cs
+++ OrderProcessor/MessageId.cs
@@ -1,8 +1,11 @@
-﻿namespace OrderProcessor.Messaging;
+﻿using System.Text.Json.Serialization;
+
+namespace OrderProcessor;
 
 public record MessageId(Guid Value) : EntityId(Value)
 {
     public static MessageId New => new(Guid.NewGuid());
 
+    [JsonIgnore]
     public OrderId ToOrderId => new(Value);
 }
